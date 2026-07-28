391f7abf5adb09a380365f4777c71f3eaf8b113f
Pashka <pavel.osincev04@mail.ru>
2026-07-28 19:24:49 +0500

    fixing a service registration error

--- PromVesServer/Program.cs
+++ PromVesServer/Program.cs
@@ -3,13 +3,18 @@ using PromVesServer.Service;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Configuration.AddJsonFile(
-    "ConfigPort.json",
-    optional: false,
-    reloadOnChange: true);
+builder.Services.AddWindowsService();
+
+builder.Configuration
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile(
+        "ConfigPort.json",
+        optional: false,
+        reloadOnChange: true);
 
 builder.Services.AddHostedService<ScaleServer>();
-
 builder.Services.AddSingleton<CounterStorageService>();
+
 var host = builder.Build();
+
 host.Run();
